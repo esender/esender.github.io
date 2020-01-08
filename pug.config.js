5b94e7f495821a2672982a74a450b1836b7f8845
Marat Abdulin <esend.work@gmail.com>
2020-01-08 20:55:35 +0300

    First implementation of Resume page. Mobile version

---
+++ pug.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  locals: {
+    resume_link: '/',
+    email: 'm​arat@esender.me',
+    linkedin_profile_link: 'https://www.linkedin.com/in/esender/',
+    telegram_link: 'https://t.me/esender',
+    twitter_link: 'https://twitter.com/esender_',
+    github_link: 'https://github.com/esender',
+  }
+};
